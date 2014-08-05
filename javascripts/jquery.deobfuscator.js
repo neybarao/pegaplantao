3a0f8dc64bbb55e7d8409d2abab052f40bb09970
Ney Ricardo <neyricardo@MacBook-Pro-de-Ney.local>
2014-08-04 23:54:39 -0400

    ok

---
+++ javascripts/jquery.deobfuscator.js
@@ -0,0 +1,56 @@
+/*!
+ * JQuery Email Deobfuscator
+ *
+ * Copyright 2011, Lim Kian Yong
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ */
+(function( $ ){ 
+
+  var settings = {
+    at: '\\[at\\]',
+    dot: '\\[dot\\]'
+  };
+  
+  var emailDomElement;
+
+  var methods = {  
+  
+    init : function( options ) { 
+      if (options) {
+        $.extend( settings, options );
+      }      
+    },
+    deobsfucate : function( ) { 
+      var configRegExp = {
+        at: new RegExp('\\s*' + settings.at + '\\s*', 'gi'),
+        dot: new RegExp('\\s*' + settings.dot + '\\s*', 'gi')
+      };
+      
+      var obfuscatedEmail = emailDomElement.attr('href');      
+      if (obfuscatedEmail) {
+        var replacedAtInEmail = obfuscatedEmail.replace(configRegExp.at, '@');
+        var replacedDotAndAtInEmail = replacedAtInEmail.replace(configRegExp.dot, '.');
+            
+        emailDomElement.attr('href', replacedDotAndAtInEmail);
+      }
+      
+      var obfuscatedEmailText = emailDomElement.text();
+      
+      var replacedAtInEmailText = obfuscatedEmailText.replace(configRegExp.at, '@');
+      var replacedDotAndAtInEmailText = replacedAtInEmailText.replace(configRegExp.dot, '.');
+          
+      emailDomElement.text(replacedDotAndAtInEmailText);
+    }
+  };
+
+  $.fn.deobsfucate = function() { 
+
+    emailDomElement = $(this);
+    methods.init.apply( this, arguments );
+    methods.deobsfucate();
+    
+    return $(this);
+  };
+
+})( jQuery );
---
+++ javascripts/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $('#myEmailID').deobsfucate();
+  
+  $('#myEmailID').deobsfucate();
+});
+
